23f357306980ffa9ecf6280b8bbd1d0f582bc4e9
Bounif Lynda <lynda.bounif@imadrassa.com>
2020-06-19 15:10:54 +0100

    extract count element from an xml webpage

---
+++ geoxml.py
@@ -0,0 +1,24 @@
+import urllib.request, urllib.parse, urllib.error
+import xml.etree.ElementTree as ET
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter location: ')
+print('Retrieving', url)
+uh = urllib.request.urlopen(url, context=ctx)
+
+data = uh.read()
+print('Retrieved', len(data), 'characters')
+tree = ET.fromstring(data)
+
+counts = tree.findall('.//count')
+print('count:', len(counts))
+total  = 0
+for item in counts:
+    count = int(item.text)
+    total = total + count
+print('Sum: ', total)
